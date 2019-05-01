769591507195802160c00e73921a12d2e9bc0f48
Jessica Gladstone <jcg321@comcast.net>
2019-05-01 15:12:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const drivers = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver() {
   drivers.push("Ralph");
 }
+
  function destructivelyPrependDriver() {
    drivers.unshift("Bob");
+ }
+
+ function destructivelyRemoveLastDriver() {
+   drivers.pop;
  }
