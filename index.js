1d141b1c9071036732b3d84f8227a25b73efbbaa
Jessica Gladstone <jcg321@comcast.net>
2019-05-01 15:25:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function destructivelyRemoveFirstDriver() {
 
 function appendDriver() {
   let drivers = ["Milo", "Otis", "Garfield"]
-  let newDrivers = [ ...drivers, "Broom"];
+  let newDrivers = [...drivers, "Broom"];
 }
